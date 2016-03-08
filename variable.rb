fa1f42a9858e80f458b502b0ef68bdac3149aeed
Abusedcode <d.ydavid@hotmail.com>
2016-03-08 23:17:54 +0000

    nitrous-backup at Tue Mar  8 23:17:54 UTC 2016

--- variable.rb
+++ variable.rb
@@ -1,4 +1,4 @@
  # Define a local variable called 'greeting',
  # give it the value of the string "Hello World"
-puts "greeting = hello world"
-puts "hello world = learn"
+myString = 'greeting'
+puts myString = 'Hellow World'
